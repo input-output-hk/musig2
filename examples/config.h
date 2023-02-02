b797befe16a5a1da6e076c008e607008fe5000d4
curiecrypt <gamze.kilic@iohk.io>
2023-02-02 19:39:51 +0300

    example enhanced

--- examples/config.h
+++ examples/config.h
@@ -1,13 +1,18 @@
 
 #define NR_SIGNERS       5 // Number of signers
-
-#define NR_MESSAGES      (const int)2    // Number of messages
-
+#define NR_MESSAGES      (const int)5   // Number of messages
 
 #define MSG_1           (const unsigned char *) "The signature for Musig2 Schnorr message 1."
-#define TAG_1           (const unsigned char *) "MSG_1"
 #define MSG_1_LEN      43
 
 #define MSG_2           (const unsigned char *) "Musig2 Schnorr MSG 2"
-#define TAG_2           (const unsigned char *) "MSG_2"
 #define MSG_2_LEN      20
+
+#define MSG_3           (const unsigned char *) "Multi-signatures enable a group of signers to produce a joint signature on a joint message"
+#define MSG_3_LEN      90
+
+#define MSG_4           (const unsigned char *) "In this work, we propose MuSig2, a simple and highly practical two-round multi-signature scheme"
+#define MSG_4_LEN      95
+
+#define MSG_5           (const unsigned char *) "We prove the security of MuSig2 in the random oracle model, and the security of a more efficient variant in the combination of the random oracle and the algebraic group model."
+#define MSG_5_LEN      175
